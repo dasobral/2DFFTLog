3367fc1a21c3a8aca6f8d86a8cf55d46845d7d58
dasobral <daniel.sobralblanco@unige.ch>
2023-11-07 20:45:27 +0100

    edit

--- cambPK.py
+++ cambPK.py
@@ -10,17 +10,23 @@ params = camb.CAMBparams()
 params.set_cosmology(**parlist['cosmology'])
 params.InitPower.set_params(**parlist['initpower'])
 #params.set_dark_energy(w=-1.0)
+
 # Calculate CAMB results for these parameter settings
+
 results = camb.get_results(params)
 
 #Now get matter power spectra and sigma8 at redshift 10
 #Note non-linear corrections couples to smaller scales than you want
+
 params.set_matter_power(**parlist['set_matterpower'])
 
 #Linear spectra
+
 params.NonLinear = model.NonLinear_none
 results = camb.get_results(params)
 kh, zlist, pk = results.get_matter_power_spectrum(**parlist['get_matterpower'])
+
 # Stack the columns
+
 pk_out = np.column_stack((kh, pk[0]))
 np.savetxt('Pkz'+str(zlist[0])+'.dat', pk_out)
